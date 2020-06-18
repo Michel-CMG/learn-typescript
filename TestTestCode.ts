5533e8bfa02b30733579547c863707ec2ce03f78
Michel-CMG <64927362+Michel-CMG@users.noreply.github.com>
2020-06-18 23:59:28 +0900

    TestTest: write the test code and test model class

---
+++ TestTestCode.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env deno test --allow-all test.ts
+
+import { assertEquals } from "https://deno.land/std@0.56.0/testing/asserts.ts";
+import { add, addAsync } from "./mod.ts";
+
+const { test } = Deno;
+
+test('example', function() {
+  const result = add(1, 2);
+  assertEquals(result, 3);
+});
+
+test('exampleAsync', async function() {
+  const result = await addAsync(1, 2);
+  assertEquals(result, 3);
+});
