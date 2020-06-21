7790bbdbb7f68f94f06e2bbf611bdcc6fab5de65
Michel-CMG <64927362+Michel-CMG@users.noreply.github.com>
2020-06-21 23:59:54 +0900

    TestCode: update function void and command

--- TestCode.ts
+++ TestCode.ts
@@ -1,9 +1,9 @@
-// deno test TestTestCode.ts
+// deno test TestCode.ts
 
 import { assertEquals } from "https://deno.land/std@0.56.0/testing/asserts.ts";
 import { add, addAsync } from "./TestClass.ts";
 
-Deno.test("TestAdd", function()
+Deno.test("TestAdd", function(): void
 {
   const result = add(1, 2);
   assertEquals(result, 3);
